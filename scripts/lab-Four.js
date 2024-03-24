ea642c7385107b63f8ac72ca6f7da75d9dfd20ab
MatthewSperling <148011258+MatthewSperling@users.noreply.github.com>
2024-03-24 15:01:54 -0600

    git is finally working

--- scripts/lab-Four.js
+++
@@ -1,9 +0,0 @@
-
-document.querySelectorAll("details.accordion").forEach((accordion) => {
-    accordion.querySelector("summary").addEventListener("click", function() {
-      this.parentElement.classList.toggle("active");
-
-    });
-  });
-
-  document.getElementById("footer-year").textContent = new Date().getFullYear();
